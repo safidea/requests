0d42a6320a096966a53df531d19aadbb74a5c94a
Thomas JEANNEAU <thomas@essentiel.io>
2024-06-15 11:11:57 +0200

    add email alert when new requests is created

--- app/automations/sendConfirmationEmail.ts
+++ app/automations/sendConfirmationEmail.ts
@@ -16,5 +16,14 @@ export const sendConfirmationEmail: Automation = {
       text: 'Thank you for your interest in Request. We will get back to you as soon as possible.',
       html: 'Thank you for your interest in Request. We will get back to you as soon as possible.',
     },
+    {
+      name: 'send-confirmation-email',
+      action: 'SendEmail',
+      from: 'noreply@safidea.com',
+      to: '$MAILER_TO',
+      subject: '{{ trigger.name }} : {{ trigger.subject }}',
+      text: '{{ trigger.email }}\n\n{{ trigger.message }}',
+      html: '{{ trigger.email }}\n\n{{ trigger.message }}',
+    },
   ],
 }
